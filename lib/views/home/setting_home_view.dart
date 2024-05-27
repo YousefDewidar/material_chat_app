e3c1ddef53fda7a59bbd3fc6c91ff7500b0f9847
yousefdewidar <ymahmoud1213@gmail.com>
2024-05-27 19:05:13 +0300

    Add picker color

--- lib/views/home/setting_home_view.dart
+++ lib/views/home/setting_home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:material_chat_app/views/setting/widgets/info_prof.dart';
 import 'package:material_chat_app/widgets/setting_card.dart';
 
@@ -15,28 +16,53 @@ class SettingHomeView extends StatelessWidget {
           style: Theme.of(context).textTheme.headlineMedium,
         ),
       ),
-        body: const Padding(
-        padding: EdgeInsets.all(24.0),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           children: [
-            InfoProf(),
+            const InfoProf(),
             SettingCard(
               cardName: 'profile',
               iconPre: Icons.person,
               iconSuf: Icons.arrow_forward_ios_rounded,
+              onTap: () {},
             ),
             SettingCard(
               cardName: 'Theme',
               iconPre: Icons.color_lens,
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Change Theme'),
+                      content: SingleChildScrollView(
+                        child: BlockPicker(
+                          pickerColor: Colors.red,
+                          onColorChanged: (v) {},
+                        ),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child:const Text('Done'))
+                      ],
+                    );
+                  },
+                );
+              },
             ),
             SettingCard(
               cardName: 'Dark mode',
               iconPre: Icons.dark_mode,
-              
+              onTap: () {},
             ),
             SettingCard(
               cardName: 'Signout',
               iconSuf: Icons.logout_outlined,
+              onTap: () {},
             ),
           ],
         ),
@@ -44,4 +70,3 @@ class SettingHomeView extends StatelessWidget {
     );
   }
 }
-
--- lib/widgets/setting_card.dart
+++ lib/widgets/setting_card.dart
@@ -4,31 +4,35 @@ class SettingCard extends StatelessWidget {
   final String cardName;
   final IconData? iconPre;
   final IconData? iconSuf;
+  final void Function() onTap;
   const SettingCard(
-      {super.key, required this.cardName, this.iconPre, this.iconSuf});
+      {super.key, required this.cardName, this.iconPre, this.iconSuf,required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(top: 10),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Icon(iconPre),
-            const SizedBox(
-              width: 10,
-            ),
-            Text(
-              cardName,
-              style: const TextStyle(fontSize: 16),
-            ),
-            const Spacer(),
-            Icon(
-              iconSuf,
-              size: 18,
-            )
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin: const EdgeInsets.only(top: 10),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Icon(iconPre),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                cardName,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const Spacer(),
+              Icon(
+                iconSuf,
+                size: 18,
+              )
+            ],
+          ),
         ),
       ),
     );
