a215e9ffdb2397bd65b222302c66c85f3eaecb9d
yousefdewidar <ymahmoud1213@gmail.com>
2024-06-09 15:19:16 +0300

    Change number of messages in chat card

--- lib/views/chat/widgets/chat_card.dart
+++ lib/views/chat/widgets/chat_card.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:material_chat_app/constant.dart';
 import 'package:material_chat_app/views/chat/in_chat_view.dart';
 
 class ChatCard extends StatelessWidget {
@@ -16,18 +18,28 @@ class ChatCard extends StatelessWidget {
           },
         ));
       },
-      child: const Card(
-        child: ListTile(
-          leading: CircleAvatar(),
-          title: Text('Name'),
-          subtitle: Text('Last message'),
-          trailing: Badge(
-            label: Text('5'),
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            largeSize: 30,
-          ),
-        ),
-      ),
+      child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection(kMessagesCollection)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Card(
+                child: ListTile(
+                  leading: const CircleAvatar(),
+                  title: const Text('Name'),
+                  subtitle: const Text('Last message'),
+                  trailing: Badge(
+                    label: Text('${snapshot.data!.docs.length}'),
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    largeSize: 30,
+                  ),
+                ),
+              );
+            } else {
+              return const SizedBox();
+            }
+          }),
     );
   }
 }
